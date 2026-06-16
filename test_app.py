f67d9266c1147dc176229f7b5788c612cbdbaa3b
Rajkumar <rajsalem06706@gmail.com>
2026-06-16 17:00:23 +0530

    updated test_App file

--- test_app.py
+++ test_app.py
@@ -1,4 +1,6 @@
-from app import appfrom app test_home():
+from app import app
+
+def test_home():
     client = app.test_client()
     response = client.get('/')
     assert response.status_code == 200
